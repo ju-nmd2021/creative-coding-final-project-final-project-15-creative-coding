5d642430057f0fc27012214c6dad2330fd45b889
DinaMo96 <93649274+DinaMostafa96@users.noreply.github.com>
2023-10-03 15:55:36 +0200

    trial 2

--- flowRoomTrial.js
+++ flowRoomTrial.js
@@ -1,6 +1,55 @@
 let resolution = 10; // Grid resolution
 let cols, rows;
 let flowfield = [];
+let particles = [];
+
+class Particle {
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.maxSpeed = 2;
+    this.prevPos = this.pos.copy();
+  }
+
+  update() {
+    // Look up the vector at the particle's current position in the flowfield
+    let x = floor(this.pos.x / resolution);
+    let y = floor(this.pos.y / resolution);
+    let force = flowfield[y][x];
+    this.applyForce(force);
+
+    // Update velocity, position, and store previous position
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+    this.edges();
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  edges() {
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+  }
+
+  show() {
+    stroke(0, 50);
+    strokeWeight(1);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+  }
+
+  updatePrev() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+  }
+}
 
 function setup() {
   createCanvas(400, 400);
@@ -11,7 +60,6 @@ function setup() {
   for (let y = 0; y < rows; y++) {
     let row = [];
     for (let x = 0; x < cols; x++) {
-      // Calculate the angle towards the center of the canvas
       let angle = atan2(
         height / 2 - y * resolution,
         width / 2 - x * resolution
@@ -21,11 +69,16 @@ function setup() {
     flowfield.push(row);
   }
 
-  background(255);
-  stroke(0, 50);
-  strokeWeight(1);
-  noFill();
+  // Create particles
+  for (let i = 0; i < 100; i++) {
+    particles.push(new Particle());
+  }
 
+  background(255);
+  noFill();
+}
+
+function draw() {
   // Draw flowfield lines
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
@@ -35,5 +88,11 @@ function setup() {
       let endY = startY + flowfield[y][x].y * resolution * 0.5;
       line(startX, startY, endX, endY);
     }
+  }
+
+  // Update and show particles
+  for (let particle of particles) {
+    particle.update();
+    particle.show();
   }
 }
